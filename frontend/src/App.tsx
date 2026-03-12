6c8f64423b4695b675ec49e0a1337f4a04bc86d4
MaedaKei <runnerkei3000@ezweb.ne.jp>
2026-03-12 22:56:01 +0900

    [test]Reactの動作確認1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
-
+/*
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +28,19 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    </>
+  )
+}
+
+export default App
+*/
+function App(){
+  //const [count,setCount]=useState(0);
+  return (
+    <>
+      <div>
+        <h1>初めてのReactだよ</h1>
+      </div>
     </>
   )
 }
