3275a3854c362d240753289b060ca7096e4ce711
Maeda_no_Dump_Desktop <runnerkei3000@ezweb.ne.jp>
2026-05-11 21:30:43 +0900

    [fix]Setting表記を修正

--- src/renderer/src/components/Menubar/MenuBar.tsx
+++ src/renderer/src/components/Menubar/MenuBar.tsx
@@ -24,7 +24,7 @@ const MENUS:MenuDef[]=[//MenuDefのリスト
         ]
     },
     {
-        label:"⚙ Setting",
+        label:"Setting",
         items:[
             {type:"item",label:"Language",onClick:()=>alert("Language")},
             {type:"separator"},
