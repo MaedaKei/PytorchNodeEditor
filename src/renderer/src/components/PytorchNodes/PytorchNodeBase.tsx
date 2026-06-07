f14c44ab744f88c035499f8f71692da041fee8eb
MaedaKei <runnerkei3000@ezweb.ne.jp>
2026-06-07 22:06:21 +0900

    [feature]Conv2dノードの仮開発を行った

--- src/renderer/src/components/PytorchNodes/PytorchNodeBase.tsx
+++ src/renderer/src/components/PytorchNodes/PytorchNodeBase.tsx
@@ -1,30 +1,55 @@
 // PytorchNodeの共通部分の定義を行う
+// ラッパーみたいなもの
 import { ReactNode } from "react";
 import { Handle,Position} from "@xyflow/react";
+import Styles from "./PytorchNodeBase.module.css";
 type PytorchNodeBaseProps={
-    NodeType:string,
     InputNum:number,
     OutputNum:number,
     children:ReactNode,//NodeTypeごとの固有の部分
 }
-export default function PytorchNodeBase({NodeType="Convolution",InputNum=1,OutputNum=1,children=<></>}:PytorchNodeBaseProps){
+export default function PytorchNodeBase({InputNum=1,OutputNum=1,children=<></>}:PytorchNodeBaseProps){
     //InputNum,OutputNumの個数に応じてHandleの数が変化する
+    /*
+    検討事項1
+    ハンドル数が増えた場合にノードの高さも動的に変動させて、ハンドルが他のハンドルとかぶらないようにしなければならない
+    各ハンドル群用となるコンテナdivを用意して制御するか、直接パーセンテージをスタイルに打ち込むか
+    divで分離して制御を採用
+    検討事項2
+    CSSモジュールに分離するか、ここにstyleを直で書くか
+    propsの部分はCSS変数をこちらから書き換えることで反映可能。
+    個人的にはここにCSSを書くとコンポーネントが見づらくなると感じている。
+    その場合、InputNumとOutputNumをCSS側にも渡す→多いほうを基準としてノードの高さを調整という流れになる
+    ハンドラは等間隔で並べるようにCSSで設定する
+    */
+    
+    //InputNumとOutputNumで大きいほうを求めておく
+    const MaxHandleNum=InputNum>OutputNum?InputNum:OutputNum;
     return (
-        <div>
-            {
-                /*InputNumに応じた個数となるtargetのHandleを描画*/
-                Array.from({length:InputNum}).map((_,i)=>(
-                    <Handle key={i} type="target" position={Position.Left}/>
-                ))
-            }
-            <p>{NodeType}</p>
-            {children}
-            {
-                /*OutputNumに応じた個数となるsourceのHandleを描画*/
-                Array.from({length:OutputNum}).map((_,i)=>(
-                    <Handle key={i} type="source" position={Position.Right}/>
-                ))
-            }
+        <div className={Styles.pytorchNodeBase} style={{"--HandleNum":MaxHandleNum} as React.CSSProperties}>
+            <div className="handleContainer">{/* targetハンドラをまとめた部分 */}
+                {
+                    /*InputNumに応じた個数となるtargetのHandleを描画*/
+                    /*個数に応じてハンドラの位置が調節されるよう、それに関するスタイルはCSSではなくこちらで設定する*/
+                    Array.from({length:InputNum}).map((_,i)=>(
+                        <Handle className="targetHandle" key={`target_${i}`} type="target" position={Position.Left}/>
+                    ))
+                }
+            </div>
+
+            <div className="nodeContentsContainer">{/*Pytorchノードのコンテンツ表示部分*/}
+                {children}
+            </div>
+
+            <div className="handleContainer">{/* sourceハンドラをまとめた部分 */}
+                {
+                    /*OutputNumに応じた個数となるsourceのHandleを描画*/
+                    /*個数に応じてハンドラの位置が調節されるよう、それに関するスタイルはCSSではなくこちらで設定する*/
+                    Array.from({length:OutputNum}).map((_,i)=>(
+                        <Handle className="sourceHandle" key={`source_${i}`} type="source" position={Position.Right}/>
+                    ))
+                }
+            </div>
         </div>
     );
 }
---
+++ src/renderer/src/components/PytorchNodes/StandardNodes/Conv2d/Conv2dNode.tsx
@@ -0,0 +1,34 @@
+import PytorchNodeBase from "../../PytorchNodeBase";
+import Styles from "./Conv2dNode.module.css";
+export default function Conv2dNode(){
+    const NodeType="Conv2d";
+    const InputNum=1;
+    const OutputNum=1;
+    /*
+    Conv2dの固有プロパティを決める
+    Pytorchのプロパティ名に準拠させる
+    プロパティを網羅するのは無理なので、プロパティテーブルから名前、値を設定できるようにするが
+    デフォルトで設定されていた方が便利なモノはこっちで一応書いておく
+    プロパティテーブルでは自由に属性名を編集したり消したりできるが、
+    絶対に削除も編集もさせたくないものをこちらに書くこととする。
+    Conv2dの場合は以下のものとした
+    */
+    //Propertyテーブルと連動させるためにこのしたのオブジェクトはuseStateとしてstoreにいくはず
+    //コード化するまでの仮のIDを設定し、これとプロパティオブジェクトを紐づける感じにしたい
+    const DefaultProperties={
+        inch:2,
+        outch:2,
+        ksize:3,
+        padding:1,
+        stride:1
+    };
+    //ノードには入出力チャンネルなどのみ表示させる
+    return (
+        <PytorchNodeBase InputNum={InputNum} OutputNum={OutputNum}>
+            <div className={Styles.Conv2dInfomation}>
+                <h3>{NodeType}</h3>
+                <h3>{`${DefaultProperties.inch} -> ${DefaultProperties.outch}`}</h3>
+            </div>
+        </PytorchNodeBase>
+    )
+}
