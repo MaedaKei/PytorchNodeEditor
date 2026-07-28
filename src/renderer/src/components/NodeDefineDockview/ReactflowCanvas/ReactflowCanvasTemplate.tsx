bf6c8b01c33c04fc7d12377fff57e02d396c19c6
MaedaKei <runnerkei3000@ezweb.ne.jp>
2026-07-28 21:57:18 +0900

    [fix]ノードのデザインを変更

--- src/renderer/src/components/NodeDefineDockview/ReactflowCanvas/ReactflowCanvasTemplate.tsx
+++ src/renderer/src/components/NodeDefineDockview/ReactflowCanvas/ReactflowCanvasTemplate.tsx
@@ -5,13 +5,19 @@ Bacgroundは背景を導入する
 ズーム、パン機能自体は標準であるっぽい
 */
 import { ReactFlow,Controls,Background,useNodesState,useEdgesState,addEdge } from "@xyflow/react";
+import { Conv2dNodeOnCanvas } from "../../PytorchNodes/StandardNodes/Conv2d/Conv2dNode";
 import "@xyflow/react/dist/style.css";
-
+/*
+カスタムノードをノードとして登録してみる
+*/
+const nodeTypes={
+    Conv2d:Conv2dNodeOnCanvas,
+}
 /* サンプルコードを実行してみる*/
 const initialNodes=[
     {
         id:"1",
-        type:"input",
+        type:"input",//片方だけのノードっぽい
         data:{label:"Start"},
         position:{x:250,y:5},
     },
@@ -21,16 +27,24 @@ const initialNodes=[
         position:{x:250,y:100},
     },
     {
+        id:"conv2d",
+        type:"Conv2d",
+        data:{label:""},
+        position:{x:350,y:150},
+    },
+    {
         id:"3",
-        type:"output",
+        type:"output",//片方だけのノードっぽい
         data:{label:"Complate"},
         position:{x:250,y:200},
-    }
+    },
+
 ];
 
 const initialEdges=[
     {id:"e1-2",source:"1",target:"2"},
-    {id:"e2-3",source:"2",target:"3"},
+    {id:"e2-conv2d",source:"2",target:"conv2d"},
+    {id:"econv2d-3",source:"conv2d",target:"3"},
 ];
 
 function FlowChart(){
@@ -44,8 +58,9 @@ function FlowChart(){
     );
     return (
         <div style={{width:"100%",height:"100%"}}>
-            <ReactFlow nodes={nodes} edges={edges} onConnect={onConnect} fitView>
-                
+            <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes} onConnect={onConnect} fitView>
+                <Controls></Controls>
+                <Background></Background>
             </ReactFlow>
         </div>
     )
--- src/renderer/src/components/PytorchNodes/PytorchNodeBase.tsx
+++ src/renderer/src/components/PytorchNodes/PytorchNodeBase.tsx
@@ -27,27 +27,27 @@ export function PytorchNodeBaseOnCanvas({InputNum=1,OutputNum=1,children=<></>}:
     const MaxHandleNum=InputNum>OutputNum?InputNum:OutputNum;
     //このPytorchNodeがどこから呼び出されたかで表示方法を変える
     return (
-        <div className={Styles.pytorchNodeBase} draggable="true" style={{"--HandleNum":MaxHandleNum} as React.CSSProperties}>
-            <div className={Styles.handleContainer}>{/* targetハンドラをまとめた部分 */}
+        <div className={Styles.pytorchNodeBase} style={{"--handleNum":MaxHandleNum} as React.CSSProperties}>
+            <div className={Styles.inputContainer}>{/* targetハンドラをまとめた部分 */}
                 {
                     /*InputNumに応じた個数となるtargetのHandleを描画*/
                     /*個数に応じてハンドラの位置が調節されるよう、それに関するスタイルはCSSではなくこちらで設定する*/
                     Array.from({length:InputNum}).map((_,i)=>(
-                        <Handle className={Styles.nodeHandle} key={`target_${i}`} type="target" position={Position.Left}/>
+                        <Handle className={Styles.nodeHandle} id={`target_${i}`} key={`target_${i}`} type="target" position={Position.Left}/>
                     ))
                 }
             </div>
-
+            
             <div className={Styles.nodeContentsContainer}>{/*Pytorchノードのコンテンツ表示部分*/}
                 {children}
             </div>
 
-            <div className={Styles.handleContainer}>{/* sourceハンドラをまとめた部分 */}
+            <div className={Styles.outputContainer}>{/* sourceハンドラをまとめた部分 */}
                 {
                     /*OutputNumに応じた個数となるsourceのHandleを描画*/
                     /*個数に応じてハンドラの位置が調節されるよう、それに関するスタイルはCSSではなくこちらで設定する*/
                     Array.from({length:OutputNum}).map((_,i)=>(
-                        <Handle className={Styles.nodeHandle} key={`source_${i}`} type="source" position={Position.Right}/>
+                        <Handle className={Styles.nodeHandle} id={`source_${i}`} key={`source_${i}`} type="source" position={Position.Right}/>
                     ))
                 }
             </div>
@@ -60,11 +60,6 @@ export function PytorchNodeBaseOnPane({InputNum=1,OutputNum=1,children=<></>}:Py
     //このPytorchNodeがどこから呼び出されたかで表示方法を変える
     return (
         <div className={Styles.pytorchNodeBase} draggable="true" style={{"--handleNum":MaxHandleNum} as React.CSSProperties}>
-
-            <div className={Styles.nodeContentsContainer}>{/*Pytorchノードのコンテンツ表示部分*/}
-                {children}
-            </div>
-            
             <div className={Styles.inputContainer}>{/* targetハンドラをまとめた部分 */}
                 {
                     /*InputNumに応じた個数となるtargetのHandleを描画*/
@@ -73,6 +68,10 @@ export function PytorchNodeBaseOnPane({InputNum=1,OutputNum=1,children=<></>}:Py
                         <div/>
                     ))
                 }
+            </div>
+
+            <div className={Styles.nodeContentsContainer}>{/*Pytorchノードのコンテンツ表示部分*/}
+                {children}
             </div>
 
             <div className={Styles.outputContainer}>{/* sourceハンドラをまとめた部分 */}
