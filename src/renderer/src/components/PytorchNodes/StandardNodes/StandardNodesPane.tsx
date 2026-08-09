6e0daac04aaba37ba6996ac3e2a2aac6c8a67848
Maeda_no_Dump_Desktop <runnerkei3000@ezweb.ne.jp>
2026-08-10 03:10:36 +0900

    [change]エッジ用のCSSを調整

--- src/renderer/src/components/PytorchNodes/StandardNodes/StandardNodesPane.tsx
+++ src/renderer/src/components/PytorchNodes/StandardNodes/StandardNodesPane.tsx
@@ -1,5 +1,5 @@
 import Styles from "./StandardNodesPane.module.css";
-import {NodeEnvContext} from "../../NodeDefineDockview/ReactflowCanvas/NodeEnvContext";
+
 /* StandardNodesのインポート*/
 import {Conv2dNodePane} from "./Conv2d/Conv2dNode";
 import {LinearNodePane} from "./Linear/LinearNode";
@@ -14,15 +14,13 @@ const StandardNodes={
 export default function StandardNodesPane(){
     /* importした標準ノードを一覧として表示する*/
     return (
-        <NodeEnvContext.Provider value={false}>{/* Pane上にあることを示す */}
-            <div className={Styles.StandardNodesPane}>
-                {
-                    /* オブジェクトに登録したノードをループで全表示させる */
-                    Object.entries(StandardNodes).map(([NodeType,NodeComponent])=>(
-                        <NodeComponent/>
-                    ))
-                }
-            </div>
-        </NodeEnvContext.Provider>
+        <div className={Styles.StandardNodesPane}>
+            {
+                /* オブジェクトに登録したノードをループで全表示させる */
+                Object.entries(StandardNodes).map(([NodeType,NodeComponent])=>(
+                    <NodeComponent/>
+                ))
+            }
+        </div>
     )
 }
